b16ae017cd9a5511ecc106b4c44b66d34f80ae2c
Jeremy Saenz <jeremy.saenz@gmail.com>
2014-03-04 22:52:17 -0800

    Got a Working clock

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/nsf/termbox-go"
 	"time"
 )
@@ -87,6 +88,30 @@ var Nine = []int{
 	0, 0, 0, 0, 1,
 }
 
+var Colon = []int{
+	0, 0, 0, 0, 0,
+	0, 0, 1, 0, 0,
+	0, 0, 0, 0, 0,
+	0, 0, 1, 0, 0,
+	0, 0, 0, 0, 0,
+}
+
+var Digits = map[rune][]int{
+	'0': Zero,
+	'1': One,
+	'2': Two,
+	'3': Three,
+	'4': Four,
+	'5': Five,
+	'6': Six,
+	'7': Seven,
+	'8': Eight,
+	'9': Nine,
+	':': Colon,
+}
+
+var End = time.Now().Add(20 * time.Minute)
+
 func main() {
 
 	termbox.Init()
@@ -119,13 +144,15 @@ loop:
 func draw() {
 	// w, h := termbox.Size()
 
+	now := time.Now()
+	m := int(End.Sub(now) / time.Minute)
+	s := int((End.Sub(now) % time.Minute) / time.Second)
+	timeLeft := fmt.Sprintf("%02d:%02d", m, s)
+
 	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
 
-	digits := [][]int{Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine}
-
-	for i, d := range digits {
-		// set chars
-		drawDigit((DigitWidth+2)*i, 0, d)
+	for i, r := range timeLeft {
+		drawDigit((DigitWidth+2)*i, 0, Digits[r])
 	}
 
 	termbox.Flush()
