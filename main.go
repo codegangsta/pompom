47e02dbb4c47aea38715c2fe83dc00caca40afa6
Jeremy Saenz <jeremy.saenz@gmail.com>
2014-03-08 11:55:23 -0800

    Handle space key. Move digits to it's own source file

--- main.go
+++ main.go
@@ -9,110 +9,6 @@ import (
 	"time"
 )
 
-const DigitWidth int = 5
-
-var Zero = []int{
-	1, 1, 1, 1, 1,
-	1, 0, 0, 0, 1,
-	1, 0, 0, 0, 1,
-	1, 0, 0, 0, 1,
-	1, 1, 1, 1, 1,
-}
-
-var One = []int{
-	0, 0, 1, 0, 0,
-	0, 1, 1, 0, 0,
-	0, 0, 1, 0, 0,
-	0, 0, 1, 0, 0,
-	0, 1, 1, 1, 0,
-}
-
-var Two = []int{
-	1, 1, 1, 1, 1,
-	0, 0, 0, 0, 1,
-	1, 1, 1, 1, 1,
-	1, 0, 0, 0, 0,
-	1, 1, 1, 1, 1,
-}
-
-var Three = []int{
-	1, 1, 1, 1, 1,
-	0, 0, 0, 0, 1,
-	1, 1, 1, 1, 1,
-	0, 0, 0, 0, 1,
-	1, 1, 1, 1, 1,
-}
-
-var Four = []int{
-	1, 0, 0, 0, 1,
-	1, 0, 0, 0, 1,
-	1, 1, 1, 1, 1,
-	0, 0, 0, 0, 1,
-	0, 0, 0, 0, 1,
-}
-
-var Five = []int{
-	1, 1, 1, 1, 1,
-	1, 0, 0, 0, 0,
-	1, 1, 1, 1, 1,
-	0, 0, 0, 0, 1,
-	1, 1, 1, 1, 1,
-}
-
-var Six = []int{
-	1, 1, 1, 1, 1,
-	1, 0, 0, 0, 0,
-	1, 1, 1, 1, 1,
-	1, 0, 0, 0, 1,
-	1, 1, 1, 1, 1,
-}
-
-var Seven = []int{
-	1, 1, 1, 1, 1,
-	0, 0, 0, 0, 1,
-	0, 0, 0, 0, 1,
-	0, 0, 0, 0, 1,
-	0, 0, 0, 0, 1,
-}
-
-var Eight = []int{
-	1, 1, 1, 1, 1,
-	1, 0, 0, 0, 1,
-	1, 1, 1, 1, 1,
-	1, 0, 0, 0, 1,
-	1, 1, 1, 1, 1,
-}
-
-var Nine = []int{
-	1, 1, 1, 1, 1,
-	1, 0, 0, 0, 1,
-	1, 1, 1, 1, 1,
-	0, 0, 0, 0, 1,
-	0, 0, 0, 0, 1,
-}
-
-var Colon = []int{
-	0, 0, 0, 0, 0,
-	0, 0, 1, 0, 0,
-	0, 0, 0, 0, 0,
-	0, 0, 1, 0, 0,
-	0, 0, 0, 0, 0,
-}
-
-var Digits = map[rune][]int{
-	'0': Zero,
-	'1': One,
-	'2': Two,
-	'3': Three,
-	'4': Four,
-	'5': Five,
-	'6': Six,
-	'7': Seven,
-	'8': Eight,
-	'9': Nine,
-	':': Colon,
-}
-
 var End = time.Now().Add(20 * time.Minute).Add(time.Second)
 var Label string
 
@@ -139,6 +35,10 @@ loop:
 			if ev.Type == termbox.EventKey && ev.Key == termbox.KeyEsc {
 				break loop
 			}
+			if ev.Type == termbox.EventKey && ev.Key == termbox.KeySpace {
+				Label = "paused"
+			}
+
 		default:
 			draw()
 			time.Sleep(100 * time.Millisecond)
